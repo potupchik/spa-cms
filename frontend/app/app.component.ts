450f871c558d67291fa22119da8d01049984e44d
Ivan Potupchik <pia.web.work@gmail.com>
2016-09-19 21:52:31 +0700

    issue #9 Обновить версию ангуляр с rc.6 на 2.0.0
    Прошел TUTORIAL для Angular 2.0

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -6,6 +6,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My SECOND Angular 2 App</h1>'
+    template: `
+      <h1>{{title}}</h1>
+      <nav>
+        <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+        <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+      </nav>
+      <router-outlet></router-outlet>
+    `,
+    styleUrls: ['app/app.component.css']
 })
-export class AppComponent { }
+
+export class AppComponent {
+    title = 'Tour of Heroes';
+}
