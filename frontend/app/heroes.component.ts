5684cfb718eeb8dc00dcfe3a8d5bc195f479d78d
potupchik <pia.web.work@gmail.com>
2016-09-19 22:01:33 +0700

    issue #9 Обновить версию ангуляр с rc.6 на 2.0.0 (#11)
    Прошел TUTORIAL по Angular 2.0

---
+++ frontend/app/heroes.component.ts
@@ -0,0 +1,63 @@
+/**
+ * Created by ivan on 17.09.16.
+ */
+
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
+import { OnInit } from '@angular/core';
+
+@Component({
+    selector: 'my-heroes',
+    templateUrl: 'app/heroes.component.html',
+    styleUrls: ['app/heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+    heroes: Hero[];
+    selectedHero: Hero;
+
+    constructor(
+        private heroService: HeroService,
+        private router: Router) {
+
+    }
+
+    add(name: string): void {
+        name = name.trim();
+        if (!name) { return; }
+        this.heroService.create(name)
+            .then(hero => {
+                this.heroes.push(hero);
+                this.selectedHero = null;
+            });
+    }
+
+    delete(hero: Hero): void {
+        this.heroService
+            .delete(hero.id)
+            .then(() => {
+                this.heroes = this.heroes.filter(h => h !== hero);
+                if (this.selectedHero === hero) { this.selectedHero = null; }
+            });
+    }
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+}
