72c41a2a67f866228a79e06f23973115a48ac89e
Ivan Potupchik <pia.web.work@gmail.com>
2016-09-19 21:57:28 +0700

    issue #9 Обновить версию ангуляр с rc.6 на 2.0.0
    Прошел TUTORIAL по Angular 2.0

---
+++ frontend/app/hero.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ivan on 17.09.16.
+ */
+"use strict";
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
---
+++ frontend/app/hero.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by ivan on 17.09.16.
+ */
+
+export class Hero {
+    id: number;
+    name: string;
+}
